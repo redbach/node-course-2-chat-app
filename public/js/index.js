28c5285ef5f84a9764cf4059d0c0e973d4776205
Charles Shephard <charles.frederic@gmail.com>
2017-09-04 15:05:34 +0200

    EMIT newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'RoyalSubject15091956',
-    text: 'Hail, My Great Leige-To-Be!'
-  });
 });
 
 socket.on('disconnect', function() {
