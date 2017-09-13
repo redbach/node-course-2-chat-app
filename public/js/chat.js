c4022e3be096c262f63500dec64efcd300498fd9
Charles Shephard <charles.frederic@gmail.com>
2017-09-13 11:43:36 +0200

    Add USERS CLASS and UpdateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,12 +16,31 @@ function scrollToBottom() {
   }
 }
 
-socket.on('connect', function() {
-  console.log('Connected to server');
+socket.on('connect', function () {
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function (err) {
+    if (err) {
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('No error with display or chat room names');
+    }
+  });
 });
 
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
+});
+
+socket.on('updateUserList', function (users) {
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
 });
 
 socket.on('newMessage', function (message) {
